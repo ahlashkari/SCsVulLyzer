f09f72789090664b9540ba5fd8b4c00fa7c20a41
Sepideh <56787489+Sepid-99@users.noreply.github.com>
2024-07-26 10:16:11 -0400

    Update analyzer.py

--- analyzer.py
+++ analyzer.py
@@ -50,7 +50,20 @@ def process_opcode(source_code):
     opcode_frequency_dic = {}
     opcodes  = compile_solidity_source(source_code)[2].split(' ')
     opcode_count = Counter(opcodes)
-    opcode_list = ['CALL', 'ADD','POP','JUMP','AND','OR','MSTORE','ISZERO','JUMPDEST','JUMPI','SLOAD','RETURN','REVERT','SUB','MLOAD','DIV','EXP','MUL','EQ','MUL','DIV','EXP','AND','OR','NOT','BYTE','ADDRESS','MSTORE','MSTORE8','SLOAD','SSTORE','JUMPI', 'INVALID', 'DUP', 'PUSH','SWAP']
+    opcode_list = ['STOP', 'ADD', 'MUL', 'SUB', 'DIV', 'SDIV', 'MOD', 'SMOD', 'ADDMOD', 'MULMOD', 'EXP', 'SIGNEXTEND',                  
+                 'LT', 'GT', 'SLT', 'SGT', 'EQ', 'ISZERO', 'AND', 'OR', 'XOR', 'NOT', 'BYTE', 'SHL', 'SHR', 'SAR',                  
+                 'SHA3', 'ADDRESS', 'BALANCE', 'ORIGIN', 'CALLER', 'CALLVALUE', 'CALLDATALOAD', 'CALLDATASIZE',                  
+                 'CALLDATACOPY', 'CODESIZE', 'CODECOPY', 'GASPRICE', 'EXTCODESIZE', 'EXTCODECOPY', 'RETURNDATASIZE',                  
+                 'RETURNDATACOPY', 'BLOCKHASH', 'COINBASE', 'TIMESTAMP', 'NUMBER', 'DIFFICULTY', 'GASLIMIT', 'POP',                  
+                 'MLOAD', 'MSTORE', 'MSTORE8', 'SLOAD', 'SSTORE', 'JUMP', 'JUMPI', 'PC', 'MSIZE', 'GAS', 'JUMPDEST',                  
+                 'PUSH1', 'PUSH2', 'PUSH3', 'PUSH4', 'PUSH5', 'PUSH6', 'PUSH7', 'PUSH8', 'PUSH9', 'PUSH10', 'PUSH11',                  
+                 'PUSH12', 'PUSH13', 'PUSH14', 'PUSH15', 'PUSH16', 'PUSH17', 'PUSH18', 'PUSH19', 'PUSH20', 'PUSH21',                  
+                 'PUSH22', 'PUSH23', 'PUSH24', 'PUSH25', 'PUSH26', 'PUSH27', 'PUSH28', 'PUSH29', 'PUSH30', 'PUSH31',                  
+                 'PUSH32', 'DUP1', 'DUP2', 'DUP3', 'DUP4', 'DUP5', 'DUP6', 'DUP7', 'DUP8', 'DUP9', 'DUP10', 'DUP11',                  
+                 'DUP12', 'DUP13', 'DUP14', 'DUP15', 'DUP16', 'SWAP1', 'SWAP2', 'SWAP3', 'SWAP4', 'SWAP5', 'SWAP6',                  
+                 'SWAP7', 'SWAP8', 'SWAP9', 'SWAP10', 'SWAP11', 'SWAP12', 'SWAP13', 'SWAP14', 'SWAP15', 'SWAP16',                  
+                 'LOG0', 'LOG1', 'LOG2', 'LOG3', 'LOG4', 'CREATE', 'CALL', 'CALLCODE', 'RETURN', 'DELEGATECALL',                  
+                 'STATICCALL', 'REVERT', 'INVALID', 'SELFDESTRUCT']
     # Remove items not in opcode_list
     keys_to_remove = set(opcode_count.keys()) - set(opcode_list)
     for key in keys_to_remove:
