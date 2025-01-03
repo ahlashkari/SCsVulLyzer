5d5c246237d826afad75adaf46f16af9ae9b512e
Sepideh <56787489+Sepid-99@users.noreply.github.com>
2025-01-03 13:07:54 -0500

    Update secondAnalyzer.py

--- SCsVulLyzer-V2.0/secondAnalyzer.py
+++ SCsVulLyzer-V2.0/secondAnalyzer.py
@@ -275,3 +275,26 @@ def analyze_solidity_contract(file_path):
     }
 
     return flatten_analysis_result(analysis_result)
+  
+def rename_out(output):
+    rename_map = {
+        'Contract Information_0': "length of contract_name",
+        'Contract Information_1': "length of base_contract",
+        'Lines of Code_0':"Number of total_lines",
+        'Lines of Code_1':"Number of code_lines",
+        'Lines of Code_2':"Number of comment_lines",
+        'Lines of Code_3':"Number of blank_lines",
+        'Solidity Features_1': 'Solidity delegatecall',
+        'Solidity Features_2': 'Solidity callcode',
+        'Solidity Features_3': 'Solidity call',
+        'Solidity Features_4': 'Solidity send',
+        'Solidity Features_5': 'Solidity transfer',
+        'Solidity Features_6': 'Solidity selfdestruct',
+        'Solidity Features_7': 'Solidity create',
+        'Functional Features_2': 'Functional_Number of "public"',
+        'Functional Features_3': 'Functional_Number of "if"',
+        'Functional Features_4': 'Functional_Number of "while"'
+    }
+    renamed_output = {rename_map.get(k, k): v for k, v in output.items()}
+    
+    return renamed_output
